054ced76e4378b3f8001e9a0c39e10f1061573a3
Ayzim90 <ayanzimina@gmail.com>
2024-05-15 00:57:45 +0400

    Homework_22_Object

---
+++ app.js
@@ -0,0 +1,50 @@
+//----------1
+/* function kokHesabla(num){
+    if(num<0){
+        console.log("Mənfi ədədlərin kökü yoxdur");
+        return;
+    }
+    let sqrtResult=Math.sqrt(num);
+    if (sqrtResult%1===0){
+        console.log(num, "Kvadrat kökü", sqrtResult);
+    }
+    else{
+        console.log(num, "üçün tam kök tapılmır");
+    }
+}
+kokHesabla(10);
+ */
+
+
+//-----------2
+
+
+/* function countVowelsConsonants(strWords){
+    strWords=strWords.toLowerCase();
+    let vowels=0;
+    let consonants=0;
+
+    for(let i=0; i<strWords.length; i++){
+        let coutn=strWords[i];
+
+        switch(count){
+            case
+        }
+
+    }
+} */
+
+
+//---------------3
+
+/* function arrJoin(myArr){
+    let result="";
+    for (let i=0;i<myArr.length; i++){
+        result+=String(myArr[i]);
+
+    }
+    return result
+}
+let resultArr=[1,"test",8,"any text"];
+let result=arrJoin(resultArr);
+console.log(result); */
